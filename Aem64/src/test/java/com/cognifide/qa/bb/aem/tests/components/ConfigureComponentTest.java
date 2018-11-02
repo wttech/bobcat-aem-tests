5ada503a427cdd9372fee23595b66dc5f43ca0c6
Daniel Madejek <daniel.madejek@cognifide.com>
2018-11-02 10:40:03 +0100

    contentfragment test

--- Aem64/src/test/java/com/cognifide/qa/bb/aem/tests/components/ConfigureComponentTest.java
+++ Aem64/src/test/java/com/cognifide/qa/bb/aem/tests/components/ConfigureComponentTest.java
@@ -80,6 +80,17 @@ public class ConfigureComponentTest extends AbstractAemAuthorTest {
             new ResourceFileLocation("component-configs/list.yaml")));
   }
 
+  @Test
+  public void configureContentFragmentComponentTest() {
+    TestPage testPage = bobcatPageFactory
+        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
+    testPage.setTitle(PAGE_TO_CREATE_TITLE);
+    assertTrue(testPage.open().isDisplayed());
+    componentController.getSidePanelAction(ConfigureComponentAction.CONFIGURE_COMPONENT_ACTION)
+        .action(new ConfigureComponentActonData("container/container", "Content Fragment", 0,
+            new ResourceFileLocation("component-configs/contentfragment.yaml")));
+  }
+
   @AfterEach
   public void deleteTestPage() throws AemPageManipulationException {
     aemTestPageControler.deleteTestPage(new SlingTestPageData(TEST_PAGE_PATH, null));
