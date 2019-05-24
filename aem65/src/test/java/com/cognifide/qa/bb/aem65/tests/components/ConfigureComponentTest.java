11994604d517ea0f1208dd7cebcb1ab885c58333
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2019-05-24 12:55:39 +0200

    Tests for AEM 6.5 fields
    - rearranged the tests and component configuration to test only specific fields; now it's easier to notice which configuration and tests correspond to specific fields
    - tweaked the logback configuration

--- aem65/src/test/java/com/cognifide/qa/bb/aem65/tests/components/ConfigureComponentTest.java
+++ aem65/src/test/java/com/cognifide/qa/bb/aem65/tests/components/ConfigureComponentTest.java
@@ -25,100 +25,194 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 
 @Modules(BobcatRunModule.class)
-@Epic("AEM 6.5 Base Tests")
-@Feature("Components")
+@Epic("AEM 6.5 authoring tests")
+@Feature("Component configuration")
+@DisplayName("Author can...")
 class ConfigureComponentTest extends AbstractAemAuthorTest {
 
   private static final String TEST_PAGE_PATH = "/content/we-retail/us/en/editcomponenttestpage";
 
   private static final String PAGE_TO_CREATE_TITLE = "testPage";
-  private static final String EXPECTED_LIST_PATTERN = "<ul><li>test test test.*</li></ul>";
-  private static final String EXPECTED_TEXT_PATTERN = ".*<b>test test test.*</b>.*";
+
+  private TestPage testPage;
 
   @BeforeEach
-  void createTestPage() throws ActionException {
+  void createAndOpenTestPage() throws ActionException {
     controller.execute(AemActions.CREATE_PAGE_VIA_SLING,
         new SlingPageData(TEST_PAGE_PATH,
             SlingDataXMLBuilder.buildFromFile("testpages/editComponentTestPage.xml")));
+
+    testPage = bobcatPageFactory.create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
+    testPage.setTitle(PAGE_TO_CREATE_TITLE);
+    assertTrue(testPage.open().isDisplayed());
   }
 
   @Test
-  @DisplayName("Text component is configured correctly")
-  void textComponentIsConfiguredCorrectly() throws ActionException {
-    TestPage testPage = bobcatPageFactory
-        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
-    testPage.setTitle(PAGE_TO_CREATE_TITLE);
-    assertTrue(testPage.open().isDisplayed());
-    controller.execute(AemActions.CONFIGURE_COMPONENT,
-        new ConfigureComponentData("container", "Text", 0,
-            new ResourceFileLocation("component-configs/text.yaml")));
-    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
-
-    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
-        .matches(EXPECTED_TEXT_PATTERN);
-  }
-
-  @Test
-  @DisplayName("List in the Text component is configured correctly")
-  void listInTextComponentIsConfiguredCorrectly() throws ActionException {
-    TestPage testPage = bobcatPageFactory
-        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
-    testPage.setTitle(PAGE_TO_CREATE_TITLE);
-    assertTrue(testPage.open().isDisplayed());
-    controller.execute(AemActions.CONFIGURE_COMPONENT,
-        new ConfigureComponentData("container", "Text", 0,
-            new ResourceFileLocation("component-configs/textlist.yaml")));
-    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
-    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
-        .matches(EXPECTED_LIST_PATTERN);
-  }
-
-  @Test
-  @DisplayName("Hero Image component is configured correctly")
-  void heroImageComponentIsConfiguredCorrectly() throws ActionException {
-    TestPage testPage = bobcatPageFactory
-        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
-    testPage.setTitle(PAGE_TO_CREATE_TITLE);
-    assertTrue(testPage.open().isDisplayed());
+  @DisplayName("configure image field")
+  void configureImage() throws ActionException {
     controller.execute(AemActions.CONFIGURE_COMPONENT,
         new ConfigureComponentData("container", "Hero Image", 0,
-            new ResourceFileLocation("component-configs/hero.yaml")));
+            new ResourceFileLocation("component-configs/hero/image.yaml")));
   }
 
   @Test
-  @DisplayName("List component is configured correctly")
-  void listComponentIsConfiguredCorrectly() throws ActionException {
-    TestPage testPage = bobcatPageFactory
-        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
-    testPage.setTitle(PAGE_TO_CREATE_TITLE);
-    assertTrue(testPage.open().isDisplayed());
+  @DisplayName("configure checkbox field")
+  void configureCheckbox() throws ActionException {
     controller.execute(AemActions.CONFIGURE_COMPONENT,
         new ConfigureComponentData("container/container", "List", 0,
-            new ResourceFileLocation("component-configs/list.yaml")));
+            new ResourceFileLocation("component-configs/list/checkbox.yaml")));
   }
 
   @Test
-  @DisplayName("Form Options component is configured correctly")
-  void formOptionsComponentIsConfiguredCorrectly() throws ActionException {
-    TestPage testPage = bobcatPageFactory
-        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
-    testPage.setTitle(PAGE_TO_CREATE_TITLE);
-    assertTrue(testPage.open().isDisplayed());
+  @DisplayName("configure textfield field")
+  void configureTextfield() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container/container", "Link Button", 0,
+            new ResourceFileLocation("component-configs/button/textfield.yaml")));
+  }
+
+  @Test
+  @DisplayName("configure multifield")
+  void configureMultifield() throws ActionException {
     controller.execute(AemActions.CONFIGURE_COMPONENT,
         new ConfigureComponentData("container/container", "Form Options", 0,
-            new ResourceFileLocation("component-configs/formoptions.yaml")));
+            new ResourceFileLocation("component-configs/formoptions/multifield.yaml")));
   }
 
   @Test
-  @DisplayName("Content Fragment component is configured correctly")
-  void contentFragmentComponentIsConfiguredCorrectly() throws ActionException {
-    TestPage testPage = bobcatPageFactory
-        .create("/editor.html" + TEST_PAGE_PATH + ".html", TestPage.class);
-    testPage.setTitle(PAGE_TO_CREATE_TITLE);
-    assertTrue(testPage.open().isDisplayed());
+  @DisplayName("configure path browser field")
+  void configurePathBrowser() throws ActionException {
     controller.execute(AemActions.CONFIGURE_COMPONENT,
         new ConfigureComponentData("container/container", "Content Fragment", 0,
-            new ResourceFileLocation("component-configs/contentfragment.yaml")));
+            new ResourceFileLocation("component-configs/contentfragment/pathbrowser.yaml")));
+  }
+
+  @Test
+  @DisplayName("configure radio group field")
+  void configureRadioGroup() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container/container", "Content Fragment", 0,
+            new ResourceFileLocation("component-configs/contentfragment/radio-group.yaml")));
+  }
+
+  @Test
+  @DisplayName("configure select field")
+  void configureSelect() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container/container", "List", 0,
+            new ResourceFileLocation("component-configs/list/select.yaml")));
+  }
+
+  @Test
+  @DisplayName("configure rich text field")
+  void configureRte() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<p>test test test<br></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: bold")
+  void configureRteBold() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-bold.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<p><b>test test test<br></b></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: italic")
+  void configureRteItalic() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-italic.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<p><i>test test test<br></i></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: lists - bullet list")
+  void configureRteListBullet() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-list-bullet.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<ul><li>test test test.*</li></ul>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: lists - numbered list")
+  void configureRteListNumbered() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-list-numbered.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<ol><li>test test test.*</li></ol>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: lists - indent")
+  void configureRteListIndent() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-list-indent.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<p style=\"margin-left: 40.0px;\">test test test<br></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: lists - outdent")
+  void configureRteListOutdent() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container/container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-list-outdent.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 1);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<p>test test test.*<br></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: hyperlink")
+  void configureRteHyperlink() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-hyperlink.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches(
+            "<p><a title=\"alternative text\" href=\"#\" target=\"_blank\">test test test<br></a></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: unlink")
+  void configureRteUnlink() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container/container", "Text", 1,
+            new ResourceFileLocation("component-configs/text/rte-unlink.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 2);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<p>test test test<br></p>");
+  }
+
+  @Test
+  @DisplayName("configure rich text options: paragraph formats")
+  void configureRteParagraphFormats() throws ActionException {
+    controller.execute(AemActions.CONFIGURE_COMPONENT,
+        new ConfigureComponentData("container", "Text", 0,
+            new ResourceFileLocation("component-configs/text/rte-paraformats.yaml")));
+    TextComponentImpl content = (TextComponentImpl) testPage.getContent(TextComponent.class, 0);
+    assertThat(content.getInnerHTML().trim().replaceAll("\\r|\\n", ""))
+        .matches("<h1>test test test<br></h1>");
   }
 
   @AfterEach
