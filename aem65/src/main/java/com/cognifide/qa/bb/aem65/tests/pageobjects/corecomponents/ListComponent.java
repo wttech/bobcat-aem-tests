3ea0fcd287ac163c169d33ad4d3fcc1570ad2636
volodymyr.vashchuk <volodymyr.vashchuk@cognifide.com>
2019-09-11 12:07:16 +0200

    Re-arrange List Component tests and it's test data again Finish tests for List Item Settings

--- aem65/src/main/java/com/cognifide/qa/bb/aem65/tests/pageobjects/corecomponents/ListComponent.java
+++ aem65/src/main/java/com/cognifide/qa/bb/aem65/tests/pageobjects/corecomponents/ListComponent.java
@@ -1,8 +1,37 @@
 package com.cognifide.qa.bb.aem65.tests.pageobjects.corecomponents;
 
-import com.cognifide.qa.bb.qualifier.PageObject;
+import java.util.ArrayList;
+import java.util.List;
 
-@PageObject
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.cognifide.qa.bb.constants.HtmlTags.Attributes;
+import com.cognifide.qa.bb.qualifier.CurrentScope;
+import com.cognifide.qa.bb.qualifier.PageObject;
+import com.google.inject.Inject;
+
+@PageObject(css = ".cmp-list")
 public class ListComponent {
+
+  @Inject
+  @CurrentScope
+  private WebElement component;
+
+  @FindBy(css = ".cmp-list__item-link")
+  private List<WebElement> linkElements;
+
+  public String getText() {
+    return component.getText();
+  }
+
+  public List<String> getLinks() {
+    List<String> links = new ArrayList<>();
+    for (WebElement element :
+        linkElements) {
+      links.add(element.getAttribute(Attributes.HREF));
+    }
+    return links;
+  }
 
 }
